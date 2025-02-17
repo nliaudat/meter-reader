3521a5e03cb0a0c39f3dcab267e67db98b758e21
Nicolas Liaudat <nliaudat@users.noreply.github.com>
2025-02-17 17:58:09 +0100

    small fixes

--- homeassistant/custom_components/meter_collector/config_flow.py
+++ homeassistant/custom_components/meter_collector/config_flow.py
@@ -50,7 +50,7 @@ class MeterCollectorOptionsFlow(config_entries.OptionsFlow):
 
     def __init__(self, config_entry: ConfigEntry):
         """Initialize options flow."""
-        self.config_entry = config_entry
+        # self.config_entry = config_entry #depreciated
 
     async def async_step_init(self, user_input=None):
         """Manage the options."""
--- homeassistant/custom_components/meter_collector/const.py
+++ homeassistant/custom_components/meter_collector/const.py
@@ -1,2 +1,2 @@
 DOMAIN = "meter_collector"
-DEFAULT_SCAN_INTERVAL = 3000  # Default scan interval in seconds
+DEFAULT_SCAN_INTERVAL = 300  # Default scan interval in seconds
